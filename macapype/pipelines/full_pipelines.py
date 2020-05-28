5b7ae0f099232fbdf96bb01cfb8819569325696f
cagna.b <bastien.CAGNA@univ-amu.fr>
2020-05-28 13:34:12 +0200

    Add T1w SPM based pipeline

--- macapype/pipelines/full_pipelines.py
+++ macapype/pipelines/full_pipelines.py
@@ -2,20 +2,19 @@
 import nipype.interfaces.utility as niu
 import nipype.pipeline.engine as pe
 
-from ..utils.utils_nodes import NodeParams
+from nipype.interfaces import fsl
+from nipype.interfaces import ants
 
-from macapype.nodes.correct_bias import T1xT2BiasFieldCorrection
-from macapype.nodes.register import IterREGBET
+from ..utils.utils_nodes import NodeParams
+from ..nodes.correct_bias import T1xT2BiasFieldCorrection
+from ..nodes.register import IterREGBET
 
 from .prepare import create_data_preparation_pipe
 from .segment import (create_old_segment_pipe,
                       create_segment_atropos_pipe)
-
 from .correct_bias import (create_masked_correct_bias_pipe,
                            create_correct_bias_pipe)
-
 from .register import create_register_NMT_pipe
-
 from .extract_brain import create_extract_pipe
 
 from macapype.utils.misc import gunzip
@@ -119,6 +118,110 @@ def create_full_T1xT2_segment_pnh_subpipes(
 
     seg_pipe.connect(reg, ('warp_file', gunzip),
                      old_segment_pipe, 'inputnode.T1')
+
+    return seg_pipe
+
+
+def create_full_spm_segmentation_pipe(
+        params_template, params={}, name='spm_based_pnh_segmentation'):
+    """ Description: SPM based segmentation pipeline from T1w and T2w images
+
+    """
+
+    # Creating pipeline
+    seg_pipe = pe.Workflow(name=name)
+
+    # Creating input node
+    inputnode = pe.Node(
+        niu.IdentityInterface(fields=['T1', 'T2']),
+        name='inputnode'
+    )
+
+    # Average_align, T1xT2BET
+    if 'data_preparation_pipe' in params.keys():
+        print("data_preparation_pipe is in params")
+        params_data_preparation_pipe = params["data_preparation_pipe"]
+    else:
+        print("*** data_preparation_pipe NOT in params")
+        params_data_preparation_pipe = {}
+
+    data_preparation_pipe = create_data_preparation_pipe(
+        params_data_preparation_pipe)
+
+    seg_pipe.connect(inputnode, 'T1', data_preparation_pipe, 'inputnode.T1')
+    seg_pipe.connect(inputnode, 'T2', data_preparation_pipe, 'inputnode.T2')
+
+    # Bias correction of cropped images
+    debias = NodeParams(T1xT2BiasFieldCorrection(), name='debias')
+    debias.load_inputs_from_dict(params["debias"])
+
+    seg_pipe.connect(data_preparation_pipe, 'denoise_T1.output_image',
+                     debias, 't1_file')
+    seg_pipe.connect(data_preparation_pipe, 'denoise_T2.output_image',
+                     debias, 't2_file')
+    seg_pipe.connect(data_preparation_pipe, 'bet_crop.mask_file',
+                     debias, 'b')
+
+    # TODO: Save PNG control of th emask image on the T1
+
+    # Iterative registration to the template
+    reg = NodeParams(IterREGBET(), name='reg')
+    reg.inputs.refb_file = params_template["template_brain"]
+    reg.load_inputs_from_dict(params["reg"])
+
+    seg_pipe.connect(debias, 't1_debiased_file', reg, 'inw_file')
+    seg_pipe.connect(debias, 't1_debiased_brain_file', reg, 'inb_file')
+
+    # TODO: Optional: redo debias 
+#    if 'redo_debias' in params.keys() and params['redo_debias']:
+
+
+    # Flirt rigid betwen T1masked and template
+    rigid_reg = NodeParams(fsl.FLIRT(), name="rigid_registration")
+    rigid_reg.inputs.df = 6
+    rigid_reg.inputs.reference = 
+    seg_pipe.connect(reg, 'warp_file', rigid_reg, 'in_file')
+
+    # Transform others images
+    files_to_register = [
+        ("T1", debias, 't1_debiased_file'),
+        ("T2", debias, 't2_debiased_file'),
+#        ("T2brain", debias, 't2_debiased_file'),
+    ]
+    reg_others = []
+    for fname, node, node_output in files_to_register:
+        reg_other = NodeParams(fsl.ApplyXFM(), name="transform_" + fname)
+        reg_other.inputs.apply_xfm = True
+        seg_pipe.connect(rigid_reg, 'out_matrix_file', reg_other, 'in_matrix_file')
+        seg_pipe.connect(node, node_output, reg_other, 'in_file')
+        reg_others.append(reg_other)
+        
+    # ANTS registration subject to template
+    reg_to_templ = NodeParams(ants.RegistrationSynQuick(), name="subject_to_template")
+    reg_to_templ.inputs.fixed_image = 
+    seg_pipe.connect(rigid_reg, 'out_file', reg_to_tmpl, 'moving_image')
+    
+    # Template files to subject (-t .... 1 to inverse matrix)
+    reg_to_sub = NodeParams(ants.RegistrationSynQuick(), name="template_to_subject")
+    reg_to_sub.inputs.moving_image = []
+    # TODO: say that ANTS need to inverse the matrix
+    reg_to_sub.inputs
+    seg_pipe.connect(rigid_reg, 'out_file', reg_to_tmpl, 'fixed_image')
+    
+    # Compute brain mask using old_segment of SPM and postprocessing on
+    # tissues' masks
+    if "old_segment_pipe" in params.keys():
+        params_old_segment_pipe = params["old_segment_pipe"]
+    else:
+        params_old_segment_pipe = {}
+
+    old_segment_pipe = create_old_segment_pipe(
+        params_template, params=params_old_segment_pipe)
+
+    seg_pipe.connect(reg, ('warp_file', gunzip),
+                     old_segment_pipe, 'inputnode.T1')
+
+    # Fill holes ?
 
     return seg_pipe
 
